7a32347e07967f2ab65f6153c46e67ee32f4a945
emanuelsolita <emanuelsolita>
2024-09-19 14:54:18 +0000

    fix 2

--- notebooks/01.2 - Silver.py
+++ notebooks/01.2 - Silver.py
@@ -51,8 +51,9 @@ target_table = 'calendar'
         .withColumn("date_start", to_date(col("date")))
       .writeStream
         .option("checkpointLocation", f"{deltaTablesDirectory}/checkpoint/calendar")
+        .option("skipChangeCommits", "true")
         .trigger(once=True)
-        .table(f"{database}.{target_schema}.{target_table}").option("skipChangeCommits", "true").awaitTermination())
+        .table(f"{database}.{target_schema}.{target_table}").awaitTermination())
 
 # COMMAND ----------
 
